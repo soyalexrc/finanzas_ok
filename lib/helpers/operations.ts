00675d1a56e72918363b048540bae01847db5ab1
alexrodriguez <alexcarvajal2404@gmail.com>
2024-10-24 23:33:16 -0500

    :zap: fix de decimales en detailGroup reports

--- lib/helpers/operations.ts
+++ lib/helpers/operations.ts
@@ -1,7 +1,7 @@
 import {ChartPoints, TransactionWithAmountNumber} from "@/lib/types/Transaction";
 
 export function calculateTotalTransactions(transactions: TransactionWithAmountNumber[], hiddenFeatureFlag: boolean) {
-    return transactions.reduce((acc, item) => acc + (hiddenFeatureFlag ? item.hidden_amount : item.amount), 0).toString()
+    return transactions.reduce((acc, item) => acc + (hiddenFeatureFlag ? item.hidden_amount : item.amount), 0).toFixed(2)
 }
 
 export function calculateTotalFromChartPoints(points: ChartPoints[], hiddenFeatureFlag: boolean) {
