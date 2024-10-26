e12107057bb21605bc7464681a2fe29e1da287ac
alexrodriguez <alexcarvajal2404@gmail.com>
2024-10-25 23:57:23 -0500

    :zap: extra validation for use biometrics

--- lib/hooks/useBiometricAuth.ts
+++ lib/hooks/useBiometricAuth.ts
@@ -1,14 +1,25 @@
-import { useState, useCallback } from 'react';
+import {useState, useCallback} from 'react';
 import * as LocalAuthentication from 'expo-local-authentication';
+import {Alert, Linking} from "react-native";
+import {useTranslation} from "react-i18next";
 
 export function useBiometricAuth() {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const {t} = useTranslation();
 
     const authenticate = useCallback(async () => {
         try {
             const hasHardwareSupport = await LocalAuthentication.hasHardwareAsync();
             if (!hasHardwareSupport) {
-                console.error('Biometric authentication is not supported on this device.');
+                Alert.alert(t('COMMON.WARNING'), 'Biometric authentication permissions are denied or not found, if you want to access the content on this page, please check out the settings and give the requested permissions.', [
+                    { text: t('COMMON.CANCEL'), style: 'destructive' },
+                    {
+                        text: t('COMMON.GIVE_ACCESS'),
+                        style: 'default',
+                        isPreferred: true,
+                        onPress: async () => await Linking.openSettings()
+                    }
+                ])
                 return false;
             }
 
