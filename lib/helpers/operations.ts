4d83d4d0fe8e3f7877b0b2845c31fa9a619b436b
alexrodriguez <alexcarvajal2404@gmail.com>
2024-10-26 00:19:50 -0500

    :zap: new operation to get totalExpenses and totalIncomes

--- lib/helpers/operations.ts
+++ lib/helpers/operations.ts
@@ -7,3 +7,10 @@ export function calculateTotalTransactions(transactions: TransactionWithAmountNu
 export function calculateTotalFromChartPoints(points: ChartPoints[], hiddenFeatureFlag: boolean) {
     return points.reduce((acc, item) => acc + (hiddenFeatureFlag ? item.total_hidden : item.total), 0).toFixed(2)
 }
+
+export function calculateTotalsFromChartPoints(points: ChartPoints[], hiddenFeatureFlag: boolean): { totalIncome: string, totalExpense: string } {
+    return {
+        totalIncome: points.reduce((acc, item) => acc + (hiddenFeatureFlag ? item.total_income_hidden : item.total_income), 0).toFixed(2),
+        totalExpense: points.reduce((acc, item) => acc + (hiddenFeatureFlag ? item.total_expense_hidden : item.total_expense), 0).toFixed(2)
+    }
+}
