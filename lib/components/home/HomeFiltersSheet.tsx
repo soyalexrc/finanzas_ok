aac20f65d6fc9e2431bef615267bd6e10d36898d
alexrodriguez <alexcarvajal2404@gmail.com>
2024-11-20 12:26:40 -0500

    :zap: red color for line

--- lib/components/home/HomeFiltersSheet.tsx
+++ lib/components/home/HomeFiltersSheet.tsx
@@ -148,7 +148,7 @@ export default function HomeFiltersSheet({setOpen, open} : Props) {
                         </XStack>
                         <XStack position="relative" height={200} justifyContent="space-between" gap={5} alignItems="flex-end" px={5}>
                             <Text position="absolute" fontSize={20} top={-28} right={10}>{convertNumberToK(limit)}</Text>
-                            <View height={1} position="absolute" borderWidth={1} borderColor={theme.color10?.val} width="100%" top={0} borderStyle="dashed" />
+                            <View height={1} position="absolute" borderWidth={1} borderColor="#F60808FF" width="100%" top={0} borderStyle="dashed" />
                             {
                                 totalByMonth.map(item => (
                                     <TouchableOpacity
