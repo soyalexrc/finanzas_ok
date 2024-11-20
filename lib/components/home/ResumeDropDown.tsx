11131fc4e14d96c3e4cb252f38be9b9f50ed31cb
alexrodriguez <alexcarvajal2404@gmail.com>
2024-11-20 18:20:50 -0500

    :zap: mejora de home resume

--- lib/components/home/ResumeDropDown.tsx
+++ lib/components/home/ResumeDropDown.tsx
@@ -1,5 +1,5 @@
 import * as DropdownMenu from "zeego/dropdown-menu";
-import {Platform, Pressable, StyleSheet} from "react-native";
+import {Alert, Dimensions, FlatList, Platform, Pressable, StyleSheet, TouchableOpacity} from "react-native";
 import {Text, useTheme, View, XStack} from 'tamagui'
 import {useAppDispatch, useAppSelector} from "@/lib/store/hooks";
 import {
@@ -15,6 +15,7 @@ import {selectAccounts, selectSelectedAccountGlobal} from "@/lib/store/features/
 import {selectSettings} from "@/lib/store/features/settings/settingsSlice";
 import {useTranslation} from "react-i18next";
 import {selectMonth, selectType, selectYear} from "@/lib/store/features/transactions/filterSlice";
+import {useState} from "react";
 
 export default function ResumeDropDown() {
     const theme = useTheme();
@@ -26,47 +27,98 @@ export default function ResumeDropDown() {
     const type = useAppSelector(selectType);
     const month = useAppSelector(selectMonth);
     const year = useAppSelector(selectYear);
+    const {width} = Dimensions.get('window');
+    const [selectedIndex, setSelectedIndex] = useState<number>(0);
+
+
+    function FirstItem() {
+        return (
+            <View style={[styles.container, {width}]}>
+                <View style={{alignItems: 'center'}}>
+                    <Text fontSize="$6">
+                        {`${type === 'expense' ? t('COMMON.SPENT_IN') : t('COMMON.INCOME_IN')}`} {month.text} {year !== new Date().getFullYear() && year}
+                    </Text>
+                    {
+                        transactionsInView.length > 0 && calculateTotal(transactionsInView, hidden_feature_flag).map((total, index) => (
+                            <XStack key={total.amount + index} mb={4} mt={index === 0 ? 10 : 0}>
+                                <Text style={[index !== 0 && {color: theme.gray10Dark.val}]}
+                                      fontSize={index === 0 ? '$9' : '$4'}>{total.symbol}</Text>
+                                <Text style={[index !== 0 && {color: theme.gray10Dark.val}]}
+                                      mt={index !== 0 ? -6 : -12}
+                                      fontSize={index === 0 ? '$12' : '$8'}>{formatByThousands(total.amount)}</Text>
+                                <Text style={[index !== 0 && {color: theme.gray10Dark.val}]}
+                                      fontSize={index === 0 ? '$9' : '$4'}>.{total.decimals}</Text>
+                            </XStack>
+                        ))
+                    }
+                    {
+                        transactionsInView.length < 1 &&
+                        <XStack mb={4}>
+                            <Text fontSize="$9">{selectedAccount.currency_symbol}</Text>
+                            <Text mt={-12}
+                                  fontSize="$12">{formatByThousands(formatWithDecimals(currentBalance).amount)}</Text>
+                            <Text fontSize="$9">.{formatWithDecimals(currentBalance).decimals}</Text>
+                        </XStack>
+                    }
+                </View>
+            </View>
+        )
+    }
+
+    const sampleData = [
+        {category: 'Food', total: 100, color: '#fefefe'},
+        {category: 'Food', total: 200, color: '#000'},
+        {category: 'Other', total: 200, color: '#FC1010FF'},
+        {category: 'Car', total: 600, color: '#7cec6f'},
+    ]
+
+
+
+
+    const data = [
+        {id: '1', content: <FirstItem/>},
+        {id: '2', content: <FirstItem/>},
+    ]
 
     return (
-        <View style={styles.container}>
-            <View style={{alignItems: 'center'}}>
-
-                <Text fontSize="$6">
-                    {`${type === 'expense' ? t('COMMON.SPENT_IN') : t('COMMON.INCOME_IN')}`} {month.text} {year !== new Date().getFullYear() && year}
-                </Text>
-                {
-                    transactionsInView.length > 0 && calculateTotal(transactionsInView, hidden_feature_flag).map((total, index) => (
-                        <XStack key={total.amount + index} mb={4} mt={index === 0 ? 10 : 0}>
-                            <Text style={[index !== 0 && {color: theme.gray10Dark.val}]}
-                                  fontSize={index === 0 ? '$9' : '$4'}>{total.symbol}</Text>
-                            <Text style={[index !== 0 && {color: theme.gray10Dark.val}]}
-                                  mt={index !== 0 ? -6 : -12}
-                                  fontSize={index === 0 ? '$12' : '$8'}>{formatByThousands(total.amount)}</Text>
-                            <Text style={[index !== 0 && {color: theme.gray10Dark.val}]}
-                                  fontSize={index === 0 ? '$9' : '$4'}>.{total.decimals}</Text>
-                        </XStack>
-                    ))
-                }
-                {
-                    transactionsInView.length < 1 &&
-                    <XStack mb={4}>
-                        <Text fontSize="$9">{selectedAccount.currency_symbol}</Text>
-                        <Text mt={-12}
-                              fontSize="$12">{formatByThousands(formatWithDecimals(currentBalance).amount)}</Text>
-                        <Text fontSize="$9">.{formatWithDecimals(currentBalance).decimals}</Text>
-                    </XStack>
-                }
+        <>
+            <FlatList
+                data={data}
+                horizontal
+                pagingEnabled
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={item => item.id}
+                renderItem={({item, index}) => item.content}
+                onMomentumScrollEnd={event => {
+                    console.log(Math.floor(event.nativeEvent.contentOffset.x))
+                    console.log(Math.floor(width))
+                    const index = Math.floor(event.nativeEvent.contentOffset.x) / Math.floor(width);
+                    setSelectedIndex(index);
+                }}
+            />
+            <View style={styles.dotsContainer}>
+                {data.map((_, index) => (
+                    <Dot key={index} isSelected={index === selectedIndex}/>
+                ))}
             </View>
-        </View>
-
+        </>
     )
 }
+
+const Dot = ({isSelected}) => (
+    <View style={[styles.dot, isSelected && styles.selectedDot]}/>
+);
 
 const styles = StyleSheet.create({
     container: {
         height: 300,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    dotsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 10
     },
     fs32: {
         fontSize: 32
@@ -85,5 +137,15 @@ const styles = StyleSheet.create({
     },
     opacityMedium: {
         opacity: 0.5
+    },
+    dot: {
+        width: 10,
+        height: 10,
+        borderRadius: 5,
+        backgroundColor: '#ccc',
+        marginHorizontal: 5
+    },
+    selectedDot: {
+        backgroundColor: '#5EAA4BFF'
     }
 })
