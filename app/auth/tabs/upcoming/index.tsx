eb1c3295c5988c6cc738880e5c8735d91738d14c
alexrodriguez <alexcarvajal2404@gmail.com>
2024-12-23 07:17:20 -0500

    :sparkles: mejora de rendimiento para futuro de calendario, consultas por mes

--- app/auth/tabs/upcoming/index.tsx
+++ app/auth/tabs/upcoming/index.tsx
@@ -1,22 +1,30 @@
-import {Button, StyleSheet, Text, View} from "react-native";
+import {Button, RefreshControl, StyleSheet, Text, TouchableWithoutFeedback, View} from "react-native";
 import auth from "@react-native-firebase/auth";
-import {Fragment, useEffect, useState} from "react";
+import {Fragment, useCallback, useEffect, useLayoutEffect, useState} from "react";
 import {AgendaList, CalendarProvider, ExpandableCalendar} from "react-native-calendars";
 import {Colors} from "@/lib/constants/colors";
 import {parse, format} from 'date-fns';
 import Fab from "@/lib/components/transactions/Fab";
 import TransactionRow from "@/lib/components/transactions/TransactionRow";
 import {MarkedDates} from "react-native-calendars/src/types";
-import {getCurrentMonth} from "@/lib/helpers/date";
+import {getCurrentMonth, getCustomMonth, getCustomMonthAndYear, getCustomMonthRangeWithYear} from "@/lib/helpers/date";
 import firestore from "@react-native-firebase/firestore";
 import {es} from "date-fns/locale";
 import {useNavigation, useRouter} from "expo-router";
 import {LocaleConfig} from 'react-native-calendars';
 import {useSafeAreaInsets} from "react-native-safe-area-context";
 import TransactionRowHeader from "@/lib/components/transactions/TransactionRowHeader";
-import Animated, {LayoutAnimationConfig, StretchInY} from "react-native-reanimated";
+import Animated, {
+    LayoutAnimationConfig,
+    StretchInY,
+    useAnimatedStyle,
+    useSharedValue,
+    withTiming
+} from "react-native-reanimated";
 import * as Haptics from "expo-haptics";
 import TransactionResumeModal from "@/lib/components/ui/modals/TransactionResumeModal";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import RNDateTimePicker from "@react-native-community/datetimepicker";
 
 
 interface Section {
@@ -53,7 +61,7 @@ LocaleConfig.defaultLocale = 'es';
 
 
 export default function Screen() {
-    const today = new Date().toISOString();
+    const today = new Date().toISOString().split('T')[0];
     const [agendaItems, setAgendaItems] = useState<Section[]>([]);
     const [markedDates, setMarkedDates] = useState<MarkedDates>({});
     const navigation = useNavigation();
@@ -61,87 +69,102 @@ export default function Screen() {
     const [modalVisible, setModalVisible] = useState<boolean>(false);
     const [selectedTransaction, setSelectedTransaction] = useState<any>({});
     const router = useRouter();
+    const [overlayVisible, setOverlayVisible] = useState(false);
+    const opacity = useSharedValue(0);
 
     useEffect(() => {
-        const {start, end} = getCurrentMonth();
+        if (overlayVisible) {
+            opacity.value = withTiming(0.5, { duration: 300 });
+        } else {
+            opacity.value = withTiming(0, { duration: 300 });
+        }
+    }, [overlayVisible]);
+
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            opacity: opacity.value,
+        };
+    });
+
+    async function getTransactionsByMonth(month?: number, year?: number) {
+        setOverlayVisible(true);
+        const {start, end} = (month && year)? getCustomMonthAndYear(month, year) : getCurrentMonth();
         const userId = auth().currentUser?.uid;
         const userRef = firestore().collection('users').doc(userId);
-        const subscriber = firestore()
+        const transactions = await firestore()
             .collection('transactions')
             .where('user_id', '==', userRef)
             .where('date', '>', start)
             .where('date', '<', end)
-            .onSnapshot(async documentSnapshot => {
-                const transactions = await Promise.all(documentSnapshot?.docs.map(async doc => {
-                    const data = doc.data();
-                    let categoryData = null;
+            .get();
 
-                    if (data.category && data.category.get) {
-                        const categoryDoc = await data.category.get();
-                        categoryData = { id: categoryDoc.id, ...categoryDoc.data() };                    }
-                    return {
-                        ...data,
-                        id: doc.id,
-                        date: doc.data().date?.toDate(),
-                        category: categoryData
-                    }
-                }));
+        const transactionsWithCategories = await Promise.all(transactions?.docs.map(async doc => {
+            const data = doc.data();
+            let categoryData = null;
 
-                const updatedMarkedDates = { ...markedDates };
+            if (data.category && data.category.get) {
+                const categoryDoc = await data.category.get();
+                categoryData = { id: categoryDoc.id, ...categoryDoc.data() };                    }
+            return {
+                ...data,
+                id: doc.id,
+                date: doc.data().date?.toDate(),
+                category: categoryData
+            }
+        }));
 
-                transactions.forEach((transaction) => {
-                    updatedMarkedDates[transaction.date.toISOString().split('T')[0]] = { marked: true, dotColor: Colors.primary };
-                });
+        const updatedMarkedDates: any = {};
 
-                setMarkedDates(updatedMarkedDates);
+        transactionsWithCategories.forEach((transaction) => {
+            updatedMarkedDates[transaction.date.toISOString().split('T')[0]] = { marked: true, dotColor: Colors.primary };
+        });
 
-                // Group tasks by day
-                const groupedByDay = transactions?.reduce((acc: { [key: string]: any[] }, transaction) => {
-                    const day = format(new Date(transaction.date || new Date()), 'd MMM · eeee', {locale: es});
-                    if (!acc[day]) {
-                        acc[day] = [];
-                    }
-                    acc[day].push(transaction);
-                    return acc;
-                }, {});
+        setMarkedDates(updatedMarkedDates);
 
-                // Convert grouped data to sections array
-                const listData: Section[] = Object.entries(groupedByDay || {}).map(([day, transactions]) => {
-                    const totals = transactions.reduce((acc: {
-                        [key: string]: { code: string, symbol: string, total: number }
-                    }, transaction) => {
-                        const {code, symbol} = transaction.currency;
-                        if (!acc[code]) {
-                            acc[code] = {code, symbol, total: 0};
-                        }
-                        acc[code].total += transaction.amount;
-                        return acc;
-                    }, {});
+        // Group tasks by day
+        const groupedByDay = transactionsWithCategories?.reduce((acc: { [key: string]: any[] }, transaction) => {
+            const day = format(new Date(transaction.date || new Date()), 'd MMM · eeee', {locale: es});
+            if (!acc[day]) {
+                acc[day] = [];
+            }
+            acc[day].push(transaction);
+            return acc;
+        }, {});
 
-                    return {
-                        title: {
-                            title: day,
-                            totals: Object.values(totals),
-                        },
-                        data: transactions,
-                    };
-                });
+        // Convert grouped data to sections array
+        const listData: Section[] = Object.entries(groupedByDay || {}).map(([day, transactions]) => {
+            const totals = transactions.reduce((acc: {
+                [key: string]: { code: string, symbol: string, total: number }
+            }, transaction) => {
+                const {code, symbol} = transaction.currency;
+                if (!acc[code]) {
+                    acc[code] = {code, symbol, total: 0};
+                }
+                acc[code].total += transaction.amount;
+                return acc;
+            }, {});
 
-
-                // Sort sections by date
-                listData.sort((a, b) => {
-                    const dateA = new Date(a.data[0].due_date || new Date());
-                    const dateB = new Date(b.data[0].due_date || new Date());
-                    return dateA.getTime() - dateB.getTime();
-                });
-
-                setAgendaItems(listData);
-            });
+            return {
+                title: {
+                    title: day,
+                    totals: Object.values(totals),
+                },
+                data: transactions,
+            };
+        });
 
 
-        // Stop listening for updates when no longer required
-        return () => subscriber();
-    }, [navigation]);
+        // Sort sections by date
+        listData.sort((a, b) => {
+            const dateA = new Date(a.data[0].due_date || new Date());
+            const dateB = new Date(b.data[0].due_date || new Date());
+            return dateA.getTime() - dateB.getTime();
+        });
+
+        setAgendaItems(listData);
+        setOverlayVisible(false);
+
+    }
 
     function manageEdit() {
         router.push('/auth/transaction-form');
@@ -169,20 +192,50 @@ export default function Screen() {
         setModalVisible(true)
     }
 
+    useEffect(() => {
+        getTransactionsByMonth();
+    }, []);
+
+    console.log(today)
+
 
     return (
         <View style={[styles.container, {paddingTop: top }]}>
+            {overlayVisible && (
+                <Animated.View style={[styles.overlay, animatedStyle]} />
+            )}
             <CalendarProvider
                 date={today}
                 showTodayButton={true}
-                todayBottomMargin={10}
+                todayBottomMargin={100}
                 todayButtonStyle={{}}
                 theme={{
                     todayButtonTextColor: '#000000',
                 }}>
                 <ExpandableCalendar
+                    // TODO: Add year selector
+                    // renderHeader={() => (
+                    //     <RNDateTimePicker
+                    //         value={new Date(today)}
+                    //         mode="date"
+                    //     />
+                    // )}
+                    firstDay={1}
+                    onMonthChange={async (date) => {
+                        console.log('month changed', date)
+                        // setToday(new Date(date.dateString).toISOString().split('T')[0]);
+                        await getTransactionsByMonth(date.month, date.year);
+                    }}
                     closeOnDayPress
                     hideArrows
+                    // onRefresh={() => console.log('refreshing...')}
+                    // refreshControl={
+                    //     <RefreshControl
+                    //         refreshing={false}
+                    //         onRefresh={() => console.log('refreshing...')}
+                    //         tintColor={Colors.primary}
+                    //     />
+                    // }
                     markedDates={markedDates}
                     theme={{
                         todayTextColor: Colors.primary,
@@ -232,5 +285,10 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         paddingHorizontal: 16,
         paddingVertical: 10,
+    },
+    overlay: {
+        ...StyleSheet.absoluteFillObject,
+        zIndex: 111,
+        backgroundColor: 'rgba(355, 355, 355, 0.9)',
     },
 });
